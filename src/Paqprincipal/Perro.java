320f6bfce3d039dbb32e2d5788b339a40615a8fa
Luis <pipe1e@hotmail.com>
2021-11-22 11:29:14 -0500

    si

--- src/Paqprincipal/Perro.java
+++ src/Paqprincipal/Perro.java
@@ -1,11 +1,81 @@
 package Paqprincipal;
 
-public class Perro extends Mascota {
-    public Perro(String color, int age, String sex) {
-        super(color, age, sex);
-    }
+public class Perro implements Mascota {
+String comida;
 
     public Perro() {
 
     }
+
+
+    public String getKind() {
+        return null;
+    }
+
+
+    public void setKind(String kind) {
+
+    }
+
+
+    public double getProfit() {
+        return 0;
+    }
+
+
+    public void setProfit(double profit) {
+
+    }
+
+
+    public String getSex() {
+        return null;
+    }
+
+
+    public void setSex(String sex) {
+
+    }
+
+
+    public String getColor() {
+        return null;
+    }
+
+
+    public void setColor(String color) {
+
+    }
+
+
+    public int getAge() {
+        return 0;
+    }
+
+
+    public void setAge(int age) {
+
+    }
+
+
+    public double getInPrice() {
+        return 0;
+    }
+
+
+    public void setInPrice(double inPrice) {
+
+    }
+
+
+    public double getOutPrice() {
+        return 0;
+    }
+
+
+    public void setOutPrice(double outPrice) {
+
+    }
+
+    private String raza;
 }
